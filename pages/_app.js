4b4c6933192302779a470cb7ba5eb256933d780f
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-06-09 21:50:31 +0100

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import MetaData from '../components/MetaData'
 import NavbarComponent from '../components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
