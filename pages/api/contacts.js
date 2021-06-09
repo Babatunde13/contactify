1b4a40a4944822b7ebc14f1264c8ee70e36b4fcb
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-06-09 23:07:42 +0100

    added protected API routes for the app

---
+++ pages/api/contacts.js
@@ -0,0 +1,32 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { withApiAuthRequired ,getSession } from "@auth0/nextjs-auth0"
+import { createContact, getContactsByUserID } from "../../models"
+
+export default withApiAuthRequired(async (req, res) => {
+    const user = getSession(req, res).user
+    if (req.method === 'POST') {
+        let newContact = await createContact(
+            ...req.body, user
+        )
+        res.status(201).json({ 
+            message: "Successfully created contact",
+            data: newContact,
+            status: 'ok'
+        })
+    } else if (req.method === 'GET') {
+        let contacts = await getContactsByUserID(user.sub)
+        res.status(200).json({ 
+            message: "Successfully retrieved contacts",
+            data: contacts,
+            status: 'ok'
+        })
+    } else {
+        res.status(405).json({
+            message: 'Method not allowed',
+            data: null,
+            sttaus: false
+        })
+    }
+})
+  
