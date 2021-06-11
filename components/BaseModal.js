d589e91db040e1ac41775540df2b5a354110ddd0
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-06-11 11:43:28 +0100

    revamped dashboard

---
+++ components/BaseModal.js
@@ -0,0 +1,70 @@
+import Modal from 'react-bootstrap/Modal'
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const BaseModal = (props)  => {
+  const onHide = () => {
+    props.updateFirstName(props.create ? '' : props.firstName)
+    props.updateLastName(props.create ? '' : props.lastName)
+    props.updateEmail(props.create ? '' : props.email)
+    props.updatePhone(props.create ? '' : props.phone)
+    props.updateAddress(props.create ? '' : props.address)
+    props.onHide()
+  }
+
+  return (
+    <Modal
+      {...props}
+      size="xlg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      onHide={onHide}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.header && props.header}
+          {props.title && props.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="show-grid">
+        <Container>
+          <Form>
+            <Row>
+              <Form.Group as={Col} className='form-group'>
+                <Form.Control placeholder="First name"  className='form-control' value={props.firstName} onChange={e => {props.updateFirstName(e.target.value)}}/>
+              </Form.Group>
+              <Form.Group as={Col} className='form-group'>
+                <Form.Control placeholder="Last name"  className='form-control' value={props.lastName} onChange={e => {props.updateLastName(e.target.value)}}/>
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group as={Col}>
+                <Form.Control type="email" placeholder="Email" value={props.email} onChange={e => {props.updateEmail(e.target.value)}}/>
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group as={Col}>
+                <Form.Control type="phone" placeholder="Phone number(+2348180854296)" value={props.phone} onChange={e => {props.updatePhone(e.target.value)}}/>
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group as={Col}>
+                <Form.Control placeholder="Address" value={props.address} onChange={e => {props.updateAddress(e.target.value)}}/>
+              </Form.Group>
+            </Row>
+          </Form>
+        </Container>
+      </Modal.Body>
+    <Modal.Footer>
+        <Button variant="danger" onClick={onHide}>Close</Button>
+        <Button variant="success" onClick={props.create ? props.handleCreate: props.handleEdit} disabled={(!props.firstName || !props.lastName || !props.phone) ? true : false}>{props.btnText}</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+
+export default BaseModal
--- components/CreateContact.modal.js
+++ components/CreateContact.modal.js
@@ -1,17 +1,10 @@
-import Modal from 'react-bootstrap/Modal'
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import BaseModal from './BaseModal'
 import { useState } from 'react'
 
 const CreateContactModal = (props)  => {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('') 
   const [email, setEmail] = useState('')
-  const [company, setCompany] = useState('') 
-  const [jobTitle, setJobTitle] = useState('') 
   const [phone, setPhone] = useState('') 
   const [address, setAddress] = useState('')
 
@@ -20,81 +13,30 @@ const CreateContactModal = (props)  => {
       firstName,
       lastName,
       email,
-      company,
-      jobTitle,
       phone, 
       address
     }
     props.onCreate(payload)
   }
 
-  const onHide = () => {
-    setFirstName('')
-    setLastName('')
-    setEmail('')
-    setPhone('')
-    setJobTitle('')
-    setAddress('')
-    setCompany('')
-    props.onHide()
-  }
-
-  return (
-    <Modal
-      {...props}
-      size="xlg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-      onHide={onHide}
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Create New Contact
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="show-grid">
-        <Container>
-          <Form>
-            <Row>
-              <Form.Group as={Col} className='form-group'>
-                <Form.Control placeholder="First name"  className='form-control' value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
-              </Form.Group>
-              <Form.Group as={Col} className='form-group'>
-                <Form.Control placeholder="Last name"  className='form-control' value={lastName} onChange={(e) => setLastName(e.target.value)}/>
-              </Form.Group>
-            </Row>
-            <Row>
-              <Form.Group as={Col}>
-                <Form.Control type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-              </Form.Group>
-            </Row>
-            <Row>
-              <Form.Group as={Col}>
-                <Form.Control type="phone" placeholder="Phone number(+2348180854296)" value={phone} onChange={(e) => setPhone(e.target.value)}/>
-              </Form.Group>
-            </Row>
-            <Row>
-              <Form.Group as={Col}>
-                <Form.Control placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)}/>
-              </Form.Group>
-            </Row>
-            <Row>
-              <Form.Group as={Col}>
-                <Form.Control placeholder="Company" value={company} onChange={(e) => setCompany(e.target.value)}/>
-              </Form.Group>
-              <Form.Group as={Col}>
-                <Form.Control placeholder="Job Title" value={jobTitle} onChange={(e) => setJobTitle(e.target.value)}/>
-              </Form.Group>
-            </Row>
-          </Form>
-        </Container>
-      </Modal.Body>
-    <Modal.Footer>
-        <Button variant="danger" onClick={props.onHide}>Close</Button>
-        <Button variant="success" onClick={handleCreate} disabled={(!firstName || !lastName || !phone) ? true : false}>Create</Button>
-      </Modal.Footer>
-    </Modal>
-  );
+  return <BaseModal
+    show={props.show}
+    onHide={props.onHide}
+    firstName={firstName}
+    lastName={lastName}
+    email={email}
+    phone={phone}
+    address={address}
+    updateFirstName={newInput => setFirstName(newInput)}
+    updateLastName={newInput => setLastName(newInput)}
+    updateEmail={newInput => setEmail(newInput)}
+    updatePhone={newInput => setPhone(newInput)}
+    updateAddress={newInput => setAddress(newInput)}
+    header="Create New Contact"
+    btnText="Create"
+    handleCreate={handleCreate}
+    create={true}
+  />
 }
 
 
