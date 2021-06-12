6a3ed93ba187e2aded9904ec90ce9f049666059e
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-06-12 15:28:16 +0100

    .

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import Image from 'next/image';
 import { useUser } from '@auth0/nextjs-auth0';
-import { useRouter } from 'next/router'
 import MetaData from '../components/MetaData'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const router = useRouter()
-  const { user, error, isLoading } = useUser();
+  const { error, isLoading } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
-  if (user) router.push('/dashboard') 
   
   return (
     <div>
