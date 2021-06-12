458e4e860cc2119763c390d8623e7c96bd69cd02
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-06-12 15:49:47 +0100

    .

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,14 +5,14 @@ import { useUser } from '@auth0/nextjs-auth0';
 import Image from 'next/image';
 
 const NavbarComponent = () => {
-  const {user} = useUser()
+  const {user, isLoading, error} = useUser()
 
   return (
     <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Brand className="mx-2 mx-md-4" href="/">Contact Manager</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse className="d-lg-flex justify-content-end" id="responsive-navbar-nav">
-          {!user ? 
+          {(!user & !isLoading & !error) ? 
             <>
               <Nav.Link className="text-light" href="api/auth/login">Sign In </Nav.Link> : 
             <Image alt="avatar" loader={myLoader} src={`https://ui-avatars.com/api/?background=random&name=John+Doe`} width="35" height="35" className="rounded-circle" />
